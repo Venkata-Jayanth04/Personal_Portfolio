108aa4b617820e2206e86597966705a9e67ac1cb
Venkata-Jayanth04 <165807855+Venkata-Jayanth04@users.noreply.github.com>
2025-11-16 01:23:20 +0530

    Update Projects.jsx

--- My_Portfolio/src/pages/Projects.jsx
+++ My_Portfolio/src/pages/Projects.jsx
@@ -6,6 +6,7 @@ export default function Projects() {
     { title: 'Brain Cancer Prediction', img: '/assets/proj3.png', github: 'https://github.com/Venkata-Jayanth04/Brain_Cancer_Prediction' },
     { title: 'Drowsiness Detection', img: '/assets/proj1.png', github: 'https://github.com/Venkata-Jayanth04/Drowsiness-Detection' },
     { title: 'Smart Contract HealthSystem', img: '/assets/proj2.png', github: 'https://github.com/Venkata-Jayanth04/Electronic-Medical-Records' },
+    { title: 'Music Genre Classifier', img: '/assets/proj4.png', github: 'https://github.com/Venkata-Jayanth04/Music-Genre-Identification-Model' },
   ]
   return (
     <main className="container projects-page">
@@ -26,3 +27,4 @@ export default function Projects() {
     </main>
   )
 }
+
